90419d222624d7826699e8a8e4958ad9c27eb418
Simon Jackson (Netbook) <jackokring@gmail.com>
2014-12-21 23:39:14 +0000

    relativity rough

--- src/com/github/jackokring/aceb/Rel.java
+++ src/com/github/jackokring/aceb/Rel.java
@@ -2,9 +2,52 @@ package com.github.jackokring.aceb;
 
 public class Rel {
 
-	float x, y;
-	float vx, vy;
+	float x[] = new float[2];
+	float vx[] = new float[2];
 	float m;
 	static final float g = 1;
 	float theta;
+	float gCalc = 1;
+	
+	static Rel rest;//object which is at rest
+	
+	public float[] vDial() {
+		float tmp[] = new float[2];
+		for(int i = 0; i < 2; i++) {
+			tmp[i] = vx[i] - rest.vx[i];
+			tmp[i] *= tmp[i];
+			tmp[i] = 1 - tmp[i];
+			tmp[i] = (float)Math.sqrt(tmp[i]);
+		}
+		return tmp;
+	}
+	
+	public float gDial() {
+		if(rest == this) return 1;
+		float tmp[] = new float[2];
+		float r[] = vDial();
+		float rr = 0;
+		float tmp2;
+		for(int i = 0; i < 2; i++) {
+			tmp[i] = x[i] - rest.x[i];
+			tmp[i] *= r[i];
+			rr += tmp[i] * tmp[i];
+		}
+		rr = (float)Math.sqrt(rr);
+		//collide? 
+		tmp2 = 1 - g * m / rr;
+		//calculate g dilate
+		return (float)Math.sqrt(tmp2);//total dilation;
+	}	
+	
+	public void gCalc() {
+		float gSum = 1;
+		Rel tmp = rest;
+		rest = this;
+		while(/*test*/) {
+			gSum *= gDial();//for each
+		}
+		rest = tmp;
+		gCalc = gSum;
+	}
 }
